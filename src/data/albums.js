5ba00b8291953ecbb2918d4a97c875ac7373cd39
Kai Brabo <kaibrabo@gmail.com>
2018-03-22 00:13:46 -0700

    added album component, displays album info

--- src/data/albums.js
+++ src/data/albums.js
@@ -1,29 +1,29 @@
-export default [{
-  title: 'The Wild Hunt',
-  artist: 'Tallest Man on Earth',
-  releaseInfo: 'Beta',
-  albumCover: '/assets/images/01.jpg',
-  slug: 'the-colors',
-  songs: [
-      { title: 'The Wild Hunt', duration: '161.71', audioSrc: '/assets/music/The Wild Hunt/01 The Wild Hunt.mp3' },
-      { title: 'Burden of Tomorrow', duration: '103.96', audioSrc: '/assets/music/The Wild Hunt/02 Burden of Tomorrow.mp3' },
-      { title: 'Troubles Will be Gone', duration: '268.45', audioSrc: '/assets/music/The Wild Hunt/03 Troubles Will be Gone.mp3' },
-      { title: "You're Going Back", duration: '153.14', audioSrc: "/assets/music/The Wild Hunt/04 You're Going Back.mp3" },
-      { title: 'The Drying of the Lawns', duration: '374.22', audioSrc: '/assets/music/The Wild Hunt/05 The Drying of the Lawns.mp3' }
-  ]
-}
-// , {
-//     title: 'The Telephone',
-//     artist: 'Guglielmo Marconi',
-//     releaseInfo: '1909 EM',
-//     albumCover: '/assets/images/album_covers/02.jpg',
-//     slug: 'the-telephone',
-//     songs: [
-//       { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-//       { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-//       { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-//       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-//       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
-//     ]
-// }
-];
+export default [
+{
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    releaseInfo: '1909 Spanish Records',
+    albumCover: '/assets/images/01.jpg',
+    slug: 'the-colors',
+    songs: [
+        { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+        { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+        { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+        { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+        { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+    ]
+},
+{
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    releaseInfo: '1909 EM',
+    albumCover: '/assets/images/01.jpg',
+    slug: 'the-telephone',
+    songs: [
+        { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+        { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+        { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+        { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+        { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+    ]
+}];
