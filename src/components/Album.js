12f1b4b00d36487157c4ba6f63a56a32a9d411e1
Kai Brabo <kaibrabo@gmail.com>
2018-03-23 00:38:58 -0700

    audio playback

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,10 +12,52 @@ class Album extends Component {
         });
 
         this.state = {
-            album: album
+            album: album,
+            currentSong: album.songs[0],
+            isPlaying: false
+        }
+
+        // audio
+        this.audioElement = document.createElement('audio');
+        // plays first song of album
+        this.audioElement.src = album.songs[0].audioSrc;
+    }
+
+    // Methods
+
+    play() {
+        this.audioElement.play();
+        this.setState({
+            isPlaying: true
+        });
+    }
+
+    pause() {
+        this.audioElement.pause();
+        this.setState({
+            isPlaying: false
+        });
+    }
+
+    setSong(song) {
+        this.audioElement.src = song.audioSrc;
+        this.setState({
+            currentSong: song
+        });
+    }
+
+    handleSongClick(song) {
+        const isSameSong = this.state.currentSong === song;
+
+        if (this.state.isPlaying && isSameSong) {
+            this.pause();
+        } else {
+            if (!isSameSong) { this.setSong(song); }
+            this.play();
         }
     }
 
+    // HTML render
     render() {
         return (
             <section className="album">
@@ -44,10 +86,14 @@ class Album extends Component {
                         <col id='song-duration-column'/>
                     </colgroup>
 
-                    <tbody className='song'>
+                    <tbody>
                         {
                             this.state.album.songs.map( (song, i) =>
-                                <tr key={i}>
+                                <tr className='song'
+                                    key={i}
+                                    onClick={
+                                        () => this.handleSongClick(song)
+                                    }>
                                     <td>
                                         {/*<i class="icon ion-play"></i>*/}
                                         {/*<i class="icon ion-pause"></i>*/}
@@ -70,8 +116,8 @@ class Album extends Component {
                     </tbody>
                 </table>
             </section>
-        )
-    }
+    )
+}
 }
 
 export default Album;
